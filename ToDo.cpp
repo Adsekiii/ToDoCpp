bc7e41b5868ab1e97144e2dcae1393fc12820b7e
Adrian Żurawski <adrianzurawski@proton.me>
2025-02-22 21:01:38 +0100

    Finished implementing functionalities

--- ToDo.cpp
+++ ToDo.cpp
@@ -14,7 +14,8 @@ ToDo::~ToDo() {
 
 void ToDo::Init() {
 
-	EditTask();
+	EraseTask();
+	AddTask();
 	ReadToDo();
 	return;
 }
@@ -54,7 +55,7 @@ void ToDo::EditTask() {
 
 	system("cls");
 	
-	std::string task = fileValue.at(2 * userChoice-1-1);
+	std::string task = fileValue.at(2 * userChoice - 1 - 1);
 	char currentChar = '\0';
 
 
@@ -92,6 +93,61 @@ void ToDo::EditTask() {
 	return;
 }
 
+void ToDo::EditStatus() {
+	std::vector<std::string> fileValue = _loader->ReadFromFile();
+
+	int userChoice = 0;
+
+	system("cls");
+	ReadToDo();
+	std::cout << "Status of which task would you like to edit? : ";
+	std::cin >> userChoice;
+	std::cin.ignore();
+	if (userChoice > fileValue.size() / 2 || userChoice < 1) {
+		std::cout << "No such task, press any key to try again!";
+		_getch();
+		EditTask();
+	}
+
+	system("cls");
+
+	std::string task = fileValue.at(2 * userChoice - 1);
+	char currentChar = '\0';
+
+
+	while (currentChar != '\r') {
+		system("cls");
+		std::cout << task;
+		currentChar = _getch();
+
+		if (currentChar == '\b') {
+			if (task.length() > 1) {
+				task.pop_back();
+			}
+			else {
+				task.at(0) = ' ';
+			}
+			continue;
+		}
+
+		if (task.at(0) == ' ') {
+			task.at(0) = currentChar;
+			continue;
+		}
+
+		task += currentChar;
+
+	}
+	//popping the new line character
+	task.pop_back();
+
+	fileValue.at(2 * userChoice - 1 ) = task;
+	_loader->ClearFile("");
+	_loader->WriteToFile(fileValue);
+	system("cls");
+	return;
+}
+
 void ToDo::ReadToDo() {
 	std::vector<std::string> fileValue= _loader->ReadFromFile();
 
@@ -104,6 +160,38 @@ void ToDo::ReadToDo() {
 		else
 			std::cout << fileValue.at(i) << '\n';
 	}
+
+	return;
+}
+
+void ToDo::EraseTask() {
+
+	std::vector<std::string> fileValue = _loader->ReadFromFile();
+	int userChoice = 0;
+
+	system("cls");
+
+	ReadToDo();
+
+	std::cout << "Which task would you like to delete: ";
+	std::cin >> userChoice;
+	std::cin.ignore();
+
+	if (userChoice > fileValue.size() / 2 || userChoice < 1) {
+		std::cout << "There is no task under that index! Press any key to try again!";
+		_getch();
+		EraseTask();
+	}
+
+	for (int i = userChoice * 2 - 1 - 1; i < fileValue.size() - 2; i++) {
+		fileValue.at(i) = fileValue.at(i+2);
+	}
+
+	fileValue.pop_back();
+	fileValue.pop_back();
+
+	_loader->ClearFile("");
+	_loader->WriteToFile(fileValue);
 
 	return;
 }
